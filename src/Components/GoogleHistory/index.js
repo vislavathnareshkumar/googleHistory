1811889acfa625b11d6405b560eb61da950cfadc
vislavathnareshkumar <vislavathnareshkumar52@gmail.com>
2023-09-05 10:42:54 +0000

    solution

---
+++ src/Components/GoogleHistory/index.js
@@ -0,0 +1,21 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class GoogleHistory extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        <div className="heading-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png"
+            className="image"
+          />
+          <div className="search-container">hello</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GoogleHistory
